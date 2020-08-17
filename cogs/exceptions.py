5fa5a67d904efca245659fe13d58891e6443ca82
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-17 16:03:06 -0400

    Added an example exception

--- cogs/exceptions.py
+++ cogs/exceptions.py
@@ -1,4 +1,18 @@
 
+"""
+Example Exception
+
+class (Exception):
+    ""Raised when ""
+
+    def __init__(self, message=""):
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+"""
+
+
 class TagAlreadyExists(Exception):
     """Raised when the inputted tag already exists"""
 
