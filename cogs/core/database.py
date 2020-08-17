d1892731c8fe3baf3a10e23a6ef7ab6c9045ee10
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-17 15:55:59 -0400

    Added add_tag method

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -1,3 +1,4 @@
+import asyncpg
 import discord
 from discord.ext import commands
 
@@ -8,6 +9,15 @@ class Database(commands.Cog):
         """Initialized the Database cog"""
 
         self.bot = bot
+
+    async def add_tag(self, tag, author):
+        """Adds a tag to the database"""
+
+        async with self.db.acquire() as conn:
+            await conn.execute(
+                "INSERT INTO tags (tag, author) VALUES ($1, $2)",
+                tag, author
+            )
 
 
 def setup(bot):
