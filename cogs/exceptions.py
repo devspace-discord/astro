eb28ec2539be06e3f1898cfcf96175ddaaebda37
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-17 16:12:47 -0400

    Added MemberAlreadyStaff Error

--- cogs/exceptions.py
+++ cogs/exceptions.py
@@ -14,9 +14,18 @@ class (Exception):
 
 
 class TagAlreadyExists(Exception):
-    """Raised when the inputted tag already exists"""
+    """Raised when the inputted tag already exists."""
 
-    def __init__(self, message="The inputted tag already exists"):
+    def __init__(self, message="The inputted tag already exists."):
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+class MemberAlreadyStaff(Exception):
+    """Raised when the inputted user is already a staff member."""
+
+    def __init__(self, message="The inputted user is already a staff member."):
         super().__init__(self.message)
 
     def __str__(self):
