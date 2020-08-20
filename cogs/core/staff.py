a5427d094e5c2c76c47d9492d20ba8a69968ffe2
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 16:23:07 -0400

    Added addstaff command

--- cogs/core/staff.py
+++ cogs/core/staff.py
@@ -7,6 +7,15 @@ class Staff(commands.Cog):
     def __init__(self, bot):
         """Initialized the Staff cog"""
         self.bot = bot
+        self.database = self.bot.get_cog("Database")
+
+    @commands.command()
+    @commands.is_owner()
+    async def add_staff(self, ctx, user: discord.User):
+        """Adds a user as to the staff member list"""
+
+        await self.database.add_staff(user.id)
+        await ctx.send(f"{self.bot.emoji["yes"]} Successfully added ``{user.name+'#'+user.discriminator}`` to the staff member list!")
 
 
 def setup(bot):
