42904395713536e2db1bd89bcecfa19f61162561
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 14:16:53 -0400

    Added make tag command

--- cogs/commands/tags.py
+++ cogs/commands/tags.py
@@ -11,12 +11,12 @@ class Tags(commands.Cog):
         self.bot = bot
         self.database = self.bot.get_cog("Database")
 
-    @commands.command(aliases=["tag_make", "create_tag"])
+    @commands.command(aliases=["create_tag"])
     @commands.check(staff_check)
     async def make_tag(self, ctx, tag, *, content):
         """Adds a custom tag to the database"""
 
-        await self.database.add_tag(tag, content, ctx.author.id)
+        await self.database.add_tag(tag.lower(), content, ctx.author.id)
         await ctx.send(f"Successfully created the ``{tag}`` tag!")
 
 
