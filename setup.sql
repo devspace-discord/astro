a0d49bc40c31ecf041b041b5c14c8d7557bd05f0
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 16:59:37 -0400

    Added reaction roles to setup.sql

--- setup.sql
+++ setup.sql
@@ -8,3 +8,10 @@ CREATE TABLE IF NOT EXISTS tags (
 CREATE TABLE IF NOT EXISTS staff (
   user_id bigint
 );
+
+CREATE TABLE IF NOT EXISTS reaction_roles (
+  reaction_role_id serial,
+  message_id bigint,
+  emoji varchar(55),
+  role_id bigint
+)
