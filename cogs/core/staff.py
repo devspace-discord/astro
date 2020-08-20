6ea1c371f5e558812fa5a2149bfc40d03a3213bf
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 13:51:30 -0400

    Added the staff check

--- cogs/core/staff.py
+++ cogs/core/staff.py
@@ -6,9 +6,11 @@ class Staff(commands.Cog):
 
     def __init__(self, bot):
         """Initialized the Staff cog"""
-
         self.bot = bot
 
+    def staff_check(ctx):
+        return ctx.author.id in self.bot.staff
+        
 
 def setup(bot):
     bot.add_cog(Staff(bot))
