ee46e29ecf4cb8dcfc8570a80e8d742284ccd244
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 15:45:18 -0400

    Added InsufficientPermissions exception

--- cogs/exceptions.py
+++ cogs/exceptions.py
@@ -51,3 +51,13 @@ class InvalidStaff(Exception):
 
     def __str__(self):
         return self.message
+
+
+class InsufficientPermissions(Exception):
+    ""Raised when a user tries to execute a command they do not have permissions for""
+
+    def __init__(self, message="A user tried to execute a command that they do not have permissions for"):
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
