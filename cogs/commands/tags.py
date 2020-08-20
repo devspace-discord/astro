0dd293a38e9a596b32a073c84dfdb8c1684f6fd0
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-20 13:51:14 -0400

    Added the staff check and make_tag command

--- cogs/commands/tags.py
+++ cogs/commands/tags.py
@@ -8,6 +8,18 @@ class Tags(commands.Cog):
         """Initialized the Tags cog"""
 
         self.bot = bot
+        self.database = self.bot.get_cog
+
+    def staff_check(ctx):
+        return ctx.author.id in ctx.bot.staff
+
+    @commands.command(aliases=["tag_make", "create_tag"])
+    @commands.check(staff_check)
+    async def make_tag(self, ctx, tag, *, content):
+        """Adds a custom tag to the database"""
+
+        await self.database.add_tag(tag, content, ctx.author.id)
+        await ctx.send(f"Successfully created the ``{tag}`` tag!")
 
 
 def setup(bot):
