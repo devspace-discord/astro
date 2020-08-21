acbbd82dd2a03a281ca5c56d6aa71d3dc275bed2
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-21 14:52:42 -0400

    Added cached variables on ready

--- cogs/core/events.py
+++ cogs/core/events.py
@@ -14,6 +14,11 @@ class Events(commands.Cog):
     async def on_ready(self):
         """Triggered when the bot is ready and it's cache is loaded"""
 
+        self.bot.guild = self.bot.get_guild(self.bot.config["guild_id"])
+        self.bot.suggestions_channel = self.bot.guild.get_channel(self.bot.config["suggestions_channel_id"])
+        self.bot.pinboard_channel = self.bot.guild.get_channel(self.bot.config["pinboard_channel_id"])
+        self.bot.error_channel_id = self.bot.guild.get_channel(self.bot.config["error_channel_id"])
+
         await self.database.update_staff()
         print(f"\n\n            ONLINE\n\n")
 
