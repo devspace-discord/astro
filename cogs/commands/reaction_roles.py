a9f91c8fdadb7238782f4f11b0779fd1ea8ea10e
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-21 16:16:37 -0400

    Added remove_reaction_role method

--- cogs/commands/reaction_roles.py
+++ cogs/commands/reaction_roles.py
@@ -20,6 +20,15 @@ class ReactionRoles(commands.Cog):
 
         await ctx.send(f"{self.bot.emoji['yes']} Successfully added reaction roles for the mentioned message!")
 
+    @commands.command()
+    @commands.check(staff_check)
+    async def remove_reaction_role(self, ctx, message_id: int, emoji: str):
+        """Removes a message's reaction role functionality"""
+
+        await self.database.remove_reaction_role(message_id, emoji)
+
+        await ctx.send(f"{self.bot.emoji['yes']} Successfully removed reaction roles for the mentioned message!")
+
 
 def setup(bot):
     bot.add_cog(ReactionRoles(bot))
